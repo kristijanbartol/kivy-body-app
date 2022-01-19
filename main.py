f1e91eecfc66dba9db048dd1f6f576e5f0f15748
kristijanbartol <kristijan.bartol@gmail.com>
2022-01-19 15:26:14 +0100

    Add more screens. Enable input. Show measurements.

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-'''
-3D Rotating Monkey Head
-========================
-
-This example demonstrates using OpenGL to display a rotating monkey head. This
-includes loading a Blender OBJ file, shaders written in OpenGL's Shading
-Language (GLSL), and using scheduled callbacks.
-
-The monkey.obj file is an OBJ file output from the Blender free 3D creation
-software. The file is text, listing vertices and faces and is loaded
-using a class in the file objloader.py. The file simple.glsl is
-a simple vertex and fragment shader written in GLSL.
-'''
-
+from tkinter import Label
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.resources import resource_find
 from kivy.graphics.transformation import Matrix
 from kivy.graphics.opengl import glEnable, glDisable, GL_DEPTH_TEST
@@ -23,51 +13,45 @@ from kivy.graphics import RenderContext, Callback, PushMatrix, PopMatrix, \
     Color, Translate, Rotate, Mesh, UpdateNormalMatrix
 from objloader import ObjFile
 
-from kivy.logger import Logger
-from kivy import kivy_data_dir
-import kivy
-from os.path import join, dirname, exists, abspath
-import sys
 import numpy as np
 import trimesh
 
 from smpl_numpy import SMPLModel
 
 
-resource_paths = ['.', dirname(sys.argv[0])]
-resource_paths += [dirname(kivy.__file__), join(kivy_data_dir, '..')]
+MEASUREMENT_NAMES = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O'
+]
 
 
-'''
-def resource_find(filename):
-    #if not filename:
-    #    return
-    #if filename[:8] == 'atlas://':
-    #    return filename
-    #if exists(abspath(filename)):
-    #    return abspath(filename)
-    for path in reversed(resource_paths):
-        output = abspath(join(path, filename))
-        if exists(output):
-            return output
-    #if filename[:5] == 'data:':
-    #    return filename
-'''
-
-
-class Renderer(Widget):
+class RenderScreen(Screen):
+    
     def __init__(self, **kwargs):
+        super(RenderScreen, self).__init__(**kwargs)
+        
         self.canvas = RenderContext(compute_normal_mat=True)
         self.canvas.shader.source = resource_find('simple.glsl')
-        #mesh_resource = resource_find("template.obj")
-
-        #smpl_model_resource = resource_find('SMPL_MALE.pkl')
+        
         smpl_model_resource = resource_find('updated_smpl_model.pkl')
 
         
         smpl = SMPLModel(smpl_model_resource)
         np.random.seed(9608)
-        pose = (np.random.rand(*smpl.pose_shape) - 0.5) * 0.4
+        pose = np.zeros(shape=smpl.pose_shape)
         beta = (np.random.rand(*smpl.beta_shape) - 0.5) * 0.06
         trans = np.zeros(smpl.trans_shape)
         smpl.set_params(beta=beta, pose=pose, trans=trans)
@@ -76,24 +60,25 @@ class Renderer(Widget):
         faces = smpl.faces.squeeze()
 
         mesh = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_colors=np.tile([.7, .7, .7], (6890, 1)))
-        #mesh.vertex_normals     # trigger @property method to calculate vertex normals
+
         vertex_normals = trimesh.geometry.weighted_vertex_normals(
             vertex_count=len(mesh.vertices),
             faces=mesh.faces,
             face_normals=mesh.face_normals,
             face_angles=mesh.face_angles)
         setattr(mesh, 'vertex_normals', vertex_normals)
-
-        #self.scene = ObjFile(mesh_resource, mode='filename')
+        
         self.scene = ObjFile(trimesh.exchange.export.export_obj(mesh))
-        super(Renderer, self).__init__(**kwargs)
+        
         with self.canvas:
             self.cb = Callback(self.setup_gl_context)
             PushMatrix()
             self.setup_scene()
             PopMatrix()
             self.cb = Callback(self.reset_gl_context)
+        
         Clock.schedule_interval(self.update_glsl, 1 / 60.)
+        
 
     def setup_gl_context(self, *args):
         glEnable(GL_DEPTH_TEST)
@@ -124,12 +109,45 @@ class Renderer(Widget):
             mode='triangles',
         )
         PopMatrix()
+        
+        
+class RenderButton(Button):
+    
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            self._calculate_measurements()
+            app.root.ids.sm.current = 'measurements_screen'
+    
+    @staticmethod
+    def _calculate_measurements():
+        try:
+            height = float(app.root.ids.height_text.text)
+            weight = float(app.root.ids.weight_text.text)
+            
+            measurements = np.array([height, weight, 1.]) @ app.male_coefs.swapaxes(0, 1) * 100.
+            
+            for midx, mvalue in enumerate(measurements):
+                app.root.ids[f'meas{midx+1}'].text = f'{MEASUREMENT_NAMES[midx]}: {mvalue:.2f}cm'
+        except ValueError:
+            pass
+        
+
+class RootWidget(Screen):
+    pass
 
 
 class RendererApp(App):
+    
     def build(self):
-        return Renderer()
+        self._prepare()
+        self.root = RootWidget()
+        return self.root
+    
+    def _prepare(self):
+        self.male_coefs = np.load('male_meas_coefs.npy')
+        #self.measurements = ['0.0cm'] * 15
 
 
 if __name__ == "__main__":
-    RendererApp().run()
+    app = RendererApp()
+    app.run()
